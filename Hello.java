94178b162a9c7dd77dbc282b90fcffa118196aa8
supriyaghuge123 <141391828+supriyaghuge123@users.noreply.github.com>
2023-08-04 11:51:44 +0530

    for Dev 2

--- Hello.java
+++ Hello.java
@@ -3,6 +3,7 @@ public class Demo
    public static void main(String args[])
 	{
 		System.out.println("This is 1st git program for Dev 1");
+		System.out.println("This is 1st git program for Dev 2");
 	}
 
 }
